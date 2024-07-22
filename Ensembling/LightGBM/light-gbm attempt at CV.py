6587c62db1ed897ed1935e775750ed67829aac69
maxrerisi <maxrerisi+git@gmail.com>
2024-07-22 15:39:51 -0400

    dont break plz

--- Ensembling/LightGBM/light-gbm attempt at CV.py
+++ Ensembling/LightGBM/light-gbm attempt at CV.py
@@ -8,6 +8,7 @@ import os
 import random
 import time
 import requests
+import itertools
 
 
 def seed_everything(seed):
@@ -157,12 +158,45 @@ for a in scr:
 print(B_A_T_AUC)
 # for some stupid reason param = {"num_leaves":31, "objective":"binary","metric":"auc"} is the best.
 
+
+def grid_search_param():
+    param_ranges = {"eta": [0.001, 0.01, 0.1, 0.5],
+                    "max_leaf": [5, 10, 15, 20, 31, 50],
+                    "min_data": [5, 10, 20, 50],
+                    "feature_fraction": [0.1, 0.25, 0.5, 0.75, 1],
+                    "reg_alpha": [0.0, 0.01, 1, 5, 25],
+                    "lambda": [0.0, 0.01, 1, 5, 25],
+                    "min_split_gain": [0, 50, 100, 250],
+                    "verbosity": -1,
+                    "max_bin": [100, 255, 500],
+                    "objective": "binary",
+                    "metric": "auc",
+                    "num_threads": 5
+                    }
+
+    constant_hyperparams = {k: v for k, v in param_ranges.items() if not isinstance(v, list)}
+    list_hyperparams = {k: v for k, v in param_ranges.items() if isinstance(v, list)}
+
+    # Generate all combinations of list hyperparameters
+    list_keys = list(list_hyperparams.keys())
+    list_values = list(list_hyperparams.values())
+    combinations = list(itertools.product(*list_values))
+    final_hyperparams = []
+    for combination in combinations:
+        config = constant_hyperparams.copy()
+        config.update(zip(list_keys, combination))
+        final_hyperparams.append(config)
+    return final_hyperparams
+
+HYPERPARAMETERS = grid_search_param()
+
 AUCs = []
 ITERS = 0
-total_amt_of_iterations = 50000
+total_amt_of_iterations = len(HYPERPARAMETERS)
 last_tick = time.time()
+B_A_T_AUC = -1
 overall_start = time.time()
-for _ in range(total_amt_of_iterations):
+for param in HYPERPARAMETERS:
     ITERS += 1
     if ITERS % 1 == 0:
         file = open("iter.txt", "w")
@@ -179,11 +213,6 @@ for _ in range(total_amt_of_iterations):
         send_ifttt_notification(f"{ITERS} / {total_amt_of_iterations} best AUC is {B_A_T_AUC}. Estim time remaining: {((time.time()-overall_start)/ITERS)*(total_amt_of_iterations-ITERS)/3600:.3f} hrs")
 
     all_preds = []
-    param = {"num_iterations": 100, "learning_rate": random.uniform(0, 1), "max_leaf": random.randint(5, 50),
-             "min_data": random.randint(0, 50), "feature_fraction": random.randint(1, 10) / 10,
-             "reg_alpha": random.uniform(0, 10), "lambda": random.uniform(0, 10),
-             "min_split_gain": random.uniform(0, 100), "verbosity": -1, "max_bin": random.randint(1, 50) * 10,
-             "objective": "binary", "metric": "auc"}
 
     for fold in range(K_FOLDS):
         start = fold * fold_size
@@ -213,7 +242,7 @@ for _ in range(total_amt_of_iterations):
     auc_val = auc(y, all_preds)
     print(auc_val)
     AUCs.append(auc_val)
-    with open("lgb_basic_data.csv","a") as file:
+    with open("grid_search_lgbm.csv","a") as file:
         str = f"\n{auc_val}"
         for val in param:
             str = f"{str},{param[val]}"
